ed651b65ceee0b560a8d1fc6cb6b5894d4f427a1
90isk <167191009+90isk@users.noreply.github.com>
2024-06-16 15:15:42 +0800

    Add files via upload

---
+++ containBroadcast/Config.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System;
+using System.ComponentModel;
+
+
+namespace ContainmentBroacastPlugin
+{
+    public sealed class Config : IConfig
+    {
+        [Description("是否启用插件")]
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
+    }
+    public class ContainPlugin : Plugin<Config>      
+    {
+        public override string Author => "123";
+        public override string Name => "123";
+        public override Version Version => new Version(1, 1, 0);
+        private ContainmentBroadcast eh { get; set; }
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            eh = new ContainmentBroadcast(Config);
+            Exiled.Events.Handlers.Player.Died += eh.Broadcast;
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Player.Died -= eh.Broadcast;
+            eh = null;
+        }
+    }
+}
---
+++ containBroadcast/ContainmentBroadcast.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+namespace ContainmentBroacastPlugin
+{
+    public class ContainmentBroadcast
+    {
+        private Config config;
+        public ContainmentBroadcast(Config config)
+        {
+            this.config = config;
+        }
+        public void Broadcast(DiedEventArgs ev)
+        {
+            if (ev.Player.IsScp == true && ev.Player.Role != RoleTypeId.Scp0492)
+            {                
+                if (ev.Attacker.IsHuman == true)
+                {
+                    var a = ev.Player.Role.Name;
+                    var b = ev.Attacker.CustomName;
+                    string message = string.Format("{0}已被重新收容 , 收容者{1}", a, b);
+                    Map.Broadcast((ushort)5f, message);
+                }
+                else
+                {
+                    var a = ev.Player.Role.Name;
+                    string message = string.Format("{0}已被重新收容 , 它自杀了", a);
+                    Map.Broadcast((ushort)5f, message);
+                }
+            }    
+        }
+    }
+}
